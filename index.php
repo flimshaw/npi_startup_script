17eba6f61f7cfe03a37286afbd6cf252f1c2c0fa
ashleylovespizza <aholtgraver@gmail.com>
2012-11-03 19:37:37 -0400

    forgot to include this file.  fuckinggit.

--- index.php
+++ index.php
@@ -1,9 +1,32 @@
 <html>
 <head>
-	<title> Hello. </title>
+	<title> Hello. Welcome to NPI!</title>
+
+	<link rel="stylesheet" type="text/css" href="index.css">
 </head>
 <body>
-	<h1>Yo.</h1>
-	<? phpinfo(); ?>
+	<header>
+		<h1>
+			<span>Welcome to NPI.</span>
+		</h1>
+	</header>
+	<div id="main">
+		<section>
+			<p>
+				We're going to connect you to the wide world of other<br/>No Pants Islands. But first, we need to get on the internet.
+			</p>
+			<p class="dominant">step 1: choose your wireless network</p>
+			<div class="wifi_choice">
+			<form>
+				<select name="mydropdown">
+					<option value="Milk">Fresh Milk</option>
+					<option value="Cheese">Old Cheese</option>
+					<option value="Bread">Hot Bread</option>
+				</select>	
+	
+				<input type="submit" value="Submit">
+			</form>
+		</section>
+	</div>
 </body>
 </html>
