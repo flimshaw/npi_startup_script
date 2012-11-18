22f31854d93eee2da2771d10e727d8782ba410ef
flimshaw <charlie.hoey@gmail.com>
2012-11-18 00:50:36 -0500

    writing to the interfaces file from the info from the form!!! just needs some cleanup now.

--- process_wifi_form.php
+++ process_wifi_form.php
@@ -3,10 +3,10 @@
 	$interfaces_template = "/etc/network/interfaces.template";
 	$interfaces_file = "/etc/network/interfaces";
 	
-	$my_network_name = "hrugle"; #$_POST['wifi_networkname'];
-	$my_network_password = "oh god"; #$_POST['wifi_password'];
-	
-	
+	$my_network_name = $_POST['wifi_networkname'];
+	$my_network_password = $_POST['wifi_password'];
+	#fancy biz.
+	exec("sudo chmod 777 /etc/network/interfaces");
 	if (is_writable($interfaces_file)) {
 	
    		# make sure your file is writable
@@ -19,17 +19,18 @@
 	   	$interfaces_newtext = implode("\n", file($interfaces_template));
 	
    		#replace the wifi and password
-   		str_replace("my_network_name", $my_network_name, $interfaces_newtext);
-   		str_replace("my_network_password", $my_network_password, $interfaces_newtext);
+   		$interfaces_newtext = str_replace("my_network_name", $my_network_name, $interfaces_newtext);
+   		$interfaces_newtext = str_replace("my_network_password", $my_network_password, $interfaces_newtext);
 
 		if (fwrite($fp, $interfaces_newtext) === FALSE) {
 			errorTown("Cannot write to the file ".$interfaces_newtext);
 			exit;
 		}
 		fclose($fp);
-
+		print $interfaces_newtext;
 		print "i'll shit myself if this works.";
 
+		exec("sudo chmod 644 /etc/network/interfaces");
    	} else {
 		errorTown("The file ".$interfaces_file." isn't writable, doofus!");
 		exit;
